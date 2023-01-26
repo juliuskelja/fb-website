f23a391555f43fbf4e155abe36c049327c9deb14
Julius <AA2850@student.jamk.fi>
2023-01-26 12:42:58 +0200

    Creating base files

---
+++ fingerbang/src/routes/Header.svelte
@@ -0,0 +1,45 @@
+
+<header>
+<nav>
+    <div class="logo">
+    </div>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/">About</a>
+        </li>
+        <li>
+            <a href="/">Support</a>
+        </li>
+    </ul>
+</nav>
+</header>
+
+<style>
+header {
+    background-color: black;
+    height: 100px;
+}
+.logo {
+    border: 1px solid white;
+    width:30px;
+}
+nav {
+    display: flex;
+    font-family: 'public pixel', sans-serif !important;
+}
+
+ul, li {
+    list-style-type:none;
+}
+ul {
+    display:flex;
+}
+li a {
+    padding-right: 1rem;
+    text-decoration: none;
+    color:white;
+}
+</style>
