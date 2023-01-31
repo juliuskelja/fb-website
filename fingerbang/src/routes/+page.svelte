f753dfd3e82b701f7a9a72708f0a72356a463718
Julius <AA2850@student.jamk.fi>
2023-01-31 14:39:53 +0200

    Updates

--- fingerbang/src/routes/+page.svelte
+++ fingerbang/src/routes/+page.svelte
@@ -7,9 +7,8 @@
 
 <article id="intro">
     <h1>Header</h1>
-    
     <p>
-    FingerBang: All Bullets Pointin' is a first person shooter game taking inspiration from the “boomer shooter” games of old. It combines elements from games both old and new, and includes a ranking system based on player performance.
+        FingerBang: All Bullets Pointin' is a first person shooter game taking inspiration from the “boomer shooter” games of old. It combines elements from games both old and new, and includes a ranking system based on player performance.
     </p>
     <p>
         You play as a former police officer taking justice in your own hands (literally), as you tear your way through the criminal element of the city, trying to put an end to the manufacturing and spreading of a dangerous new drug known as Strutusol.
@@ -17,12 +16,9 @@
 </article>
 
 <article id="screenshots">
+    <h1>Screenshots</h1>
 <Carousel /> 
 </article>
 
-<article id="mail">
-    <h1>Subscribe to our mailing list</h1>
-    <p>...</p>
-</article>
 
 
--- fingerbang/src/routes/Header.svelte
+++ fingerbang/src/routes/Header.svelte
@@ -1,25 +1,47 @@
 <header>
     
 <nav>
+    <!--Logo-->
     <div id="headerlogo">
     <img alt="Fingerbang logo" src="/images/community_icon_black.jpg" style="height:50px"/>
     </div>
-            <div class="navitem">
-                <a href="/">Home</a>
+    <!--Navigation links-->
+        <div class="navitem">
+            <a href="/">Home</a>
+        </div>
+        <div class="navitem">
+            <a href="/about">About</a>
+        </div>
+        <div class="navitem">
+            <a href="/news">News</a>
+        </div>
+        <div class="navitem">
+            <a href="/support">Support</a>
+        </div>
+    <!--Social medias
+        <div id="socialmedias">
+            <div class="smedia-item">
+                <img src="/images/yt_logo.png" alt="Youtube icon">
             </div>
-            <div class="navitem">
-                <a href="/">About</a>
+            <div class="smedia-item">
+                <img src="/images/fb_logo.png" alt="Facebook icon">
             </div>
-            <div class="navitem">
-                <a href="/">Support</a>
+            <div class="smedia-item">
+                <img src="/images/twt_logo.png" alt="Twitter icon">
             </div>
-            <div class="playButtonDiv">
-            <a href="/">
-                <div class="playfreeButton">
-                    Play Free
-                </div>
-            </a>
+            <div class="smedia-item">
+                <img src="/images/linkedin_logo.png" alt="LinkedIn icon">
             </div>
+        </div>-->
+
+    <!--Play Free button-->
+        <div class="playButtonDiv">
+        <a href="https://store.steampowered.com/app/2200380/Fingerbang_All_Bullets_Pointin/" >
+            <div class="playfreeButton">
+                Play Free
+            </div>
+        </a>
+        </div>
 </nav>
 </header>
 
@@ -63,6 +85,18 @@ nav {
     font-size: 1.2rem;
     font-weight: 600;
     -webkit-text-stroke: 1px black;
+}
+
+#socialmedias {
+    display:flex;
+    flex-direction: row;
+    right:350px;
+    position:absolute;
+}
+
+.smedia-item {
+    width:35px;
+    margin:4px;
 }
 
 #headerlogo {
