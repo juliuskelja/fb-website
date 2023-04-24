cd9a66227550dcc3e85d235c1225329580f6d3b3
Julius <AA2850@student.jamk.fi>
2023-04-24 14:03:26 +0300

    rss-feed added

--- fingerbang/fingerbang/src/routes/news/+page.svelte
+++ fingerbang/fingerbang/src/routes/news/+page.svelte
@@ -2,5 +2,8 @@
 
 <div class="contents normal">
 <h1>What's New?</h1>
-<article>...</article>
+<article>Get the latest patch notes from Steam!</article>
+
+<!--RSS Feed-->
+<rssapp-list id="rCkkO8J8FZr2qdBI"></rssapp-list><script src="https://widget.rss.app/v1/list.js" type="text/javascript" async></script>
 </div>
