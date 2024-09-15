f60b4b9009a459b0a8e6404b68ff394a2951cf1e
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2024-09-15 14:36:19 +0300

    dfs

--- fingerbang/fingerbang/svelte.config.js
+++ fingerbang/fingerbang/svelte.config.js
@@ -1,15 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        appDir: 'app', // Required as the default is _app
+        adapter: adapter()
+    },
+    preprocess: vitePreprocess()
 };
+export default config;
