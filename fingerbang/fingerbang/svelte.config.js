fdd3e2a5e4cf6ac3bd7a6502639591dcf96471ab
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2024-09-15 15:30:39 +0300

    das

--- fingerbang/fingerbang/svelte.config.js
+++ fingerbang/fingerbang/svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/fb-website' : '',
+			base: process.env.NODE_ENV === 'production' ? '/fingerbang.github.io' : '',
 		}
 	}
 };
