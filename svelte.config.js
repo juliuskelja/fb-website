9c9f075b79c023b307e2a397b488d11ea5805e7e
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2024-09-15 17:39:21 +0300

    asd

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 				fallback: 'app.html'
 			}),
 			paths: {
-					base: process.env.NODE_ENV === 'production' ? '/fb-website' : '',
+					base: process.env.NODE_ENV === 'production' ? '/fb-website/build' : '',
 			}
 	}
 };
